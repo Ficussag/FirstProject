97a2e751eb5b8936abce92f58adc9ac004b240bb
Ficussag <see1sawkuwraw@gmail.com>
2024-04-12 20:27:02 +0300

    messed up the code

--- zmeika.cpp
+++ zmeika.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
-using namespace std;
+sing namespace std;
 
 bool gameOver;
 const int width = 20;
@@ -9,7 +9,7 @@ const int height = 20;
 int x, y, fruitX, fruitY, score;
 int tailX[100], tailY[100];
 int nTail;
-enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN };
+enum eirecton { STOP = 0, LEFT, RIGHT, UP, DOWN };
 eDirecton dir;
 
 void Setup()
