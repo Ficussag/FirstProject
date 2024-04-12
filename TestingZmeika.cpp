ea577c099d48bea9c316862f3a19319b3d04ed29
nikit <nikita.vorontsov.1989@mail.ru>
2024-04-12 21:47:01 +0300

    New X, Y values

--- TestingZmeika.cpp
+++ TestingZmeika.cpp
@@ -7,7 +7,7 @@ bool gameOver;
 const int width = 20;
 const int height = 20;
 int x, y, fruitX, fruitY, score;
-int tailX[100], tailY[100];
+int tailX[1000], tailY[1000];
 int nTail;
 
 enum eDirecton { STOP = 0, LEFT, RIGHT, UP, DOWN };
