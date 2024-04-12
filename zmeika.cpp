5b334835aa0b44ef5314246c120e5b1c411dd59c
Ficussag <see1sawkuwraw@gmail.com>
2024-04-12 20:23:31 +0300

    head change 3

--- zmeika.cpp
+++ zmeika.cpp
@@ -37,7 +37,7 @@ void Draw()
             if (j == 0)
                 cout << "#";
             if (i == y && j == x)
-                cout << "0";     // head
+                cout << "O";     // head
             else if (i == fruitY && j == fruitX)
                 cout << "$";         // apple
             else
