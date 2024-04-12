af92d78d4c406b0a8f02fe5947504f2fbe55e209
Ficussag <see1sawkuwraw@gmail.com>
2024-04-12 20:10:15 +0300

    added the snake

--- zmeika.cpp
+++ zmeika.cpp
@@ -35,11 +35,11 @@ void Draw()
         for (int j = 0; j < width; j++)
         {
             if (j == 0)
-                cout << "#";
+                cout << "#";      
             if (i == y && j == x)
-                cout << "@";
+                cout << "8";     // голова
             else if (i == fruitY && j == fruitX)
-                cout << "*";
+                cout << "r";         // яблоки 
             else
             {
                 bool print = false;
@@ -47,7 +47,7 @@ void Draw()
                 {
                     if (tailX[k] == j && tailY[k] == i)
                     {
-                        cout << "^"; print = true;
+                        cout << "5"; print = true; // тело
                     }
                 }
                 if (!print) cout << " ";
@@ -64,4 +64,94 @@ void Draw()
         cout << "#";
     cout << endl;
     cout << "Score:" << score << endl;
+}
+
+void Input()
+{
+    if (_kbhit())
+    {
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
+}
+
+void algorithm()
+{
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    default:
+        break;
+    }
+    if (x >= width)
+        x = 0; else if (x < 0) x = width - 1;
+    if (y >= height)
+        y = 0; else if (y < 0) y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+
+int main()
+{
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        algorithm();
+        Sleep(50); // Добавляем задержку, чтобы замедлить движение змейки
+    }
+    return 0;
 }
